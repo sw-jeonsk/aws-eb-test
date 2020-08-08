ff7f8541fc99c5bf15b2608d270389fa0e6b56a9
sk.jeon <sk.jeon@a-in.co>
2020-08-08 14:45:49 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'jsk' });
+  res.render('index', { title: 'jsktest' });
 });
 
 module.exports = router;
