8e5313e23343f39104908ad56b73088c527b3a0c
sk.jeon <sk.jeon@a-in.co>
2020-08-08 12:54:31 +0900

    test

--- tests/test_world.js
+++ tests/test_world.js
@@ -1,11 +1,13 @@
 assert = require("assert")
 
+// user test
 describe('Test suite', function () {
   it('should be ok', function () {
     assert.equal(true, true);
   });
 });
 
+// function api test
 describe('Test suite', function () {
   it('should be ok2', function () {
     assert.equal(true, true);
